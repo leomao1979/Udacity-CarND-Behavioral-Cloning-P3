8083a90b363948baa42306f871bfd2e8f66e14d1
Chaoliu Mao <chaolium@telenav.com>
2017-09-01 01:30:09 -0700

    Added Nvidia model

--- clone.py
+++ clone.py
@@ -22,7 +22,7 @@ def load_driving_data():
 
     images = []
     steerings = []
-    steering_adjustment = 0.1
+    steering_adjustment = 0.2
     for line in lines:
         center_image_path = image_path(line[0])
         images.append(cv2.imread(center_image_path))
@@ -53,24 +53,39 @@ def build_simple_model():
     model.add(Dense(1))
     return model
 
-def resize_normalize(image):
+def resize_normalize(image, new_size):
     from keras.backend import tf
-    resized = tf.image.resize_images(image, (32, 32))
+    resized = tf.image.resize_images(image, new_size)
     normalized = resized / 255.0 - 0.5
     return normalized
 
 def build_lenet_model():
     model = Sequential()
     model.add(Cropping2D(cropping = ((75, 25), (0, 0)), input_shape = (160, 320, 3)))
-    model.add(Lambda(resize_normalize))
+    model.add(Lambda(resize_normalize, arguments = {'new_size': (32, 32)}))
     model.add(Conv2D(6, (5, 5), activation = 'relu'))
     model.add(MaxPooling2D())
     model.add(Conv2D(16, (5, 5), activation = 'relu'))
     model.add(MaxPooling2D())
     model.add(Flatten())
-    # TBD: dropout
     model.add(Dense(120))
     model.add(Dense(84))
+    model.add(Dense(1))
+    return model
+
+def build_nvidia_model():
+    model = Sequential()
+    model.add(Cropping2D(cropping = ((75, 25), (0, 0)), input_shape = (160, 320, 3)))
+    model.add(Lambda(resize_normalize, arguments = {'new_size': (66, 200)}))
+    model.add(Conv2D(24, kernel_size = (5, 5), strides=(2, 2), activation = 'relu'))
+    model.add(Conv2D(36, kernel_size = (5, 5), strides=(2, 2), activation = 'relu'))
+    model.add(Conv2D(48, kernel_size = (5, 5), strides=(2, 2), activation = 'relu'))
+    model.add(Conv2D(64, kernel_size = (3, 3), activation = 'relu'))
+    model.add(Conv2D(64, kernel_size = (3, 3), activation = 'relu'))
+    model.add(Flatten())
+    model.add(Dense(100))
+    model.add(Dense(50))
+    model.add(Dense(10))
     model.add(Dense(1))
     return model
 
@@ -80,7 +95,7 @@ X_train = np.array(images)
 y_train = np.array(steerings)
 print('X_train.shape: {}, y_train.shape: {}'.format(X_train.shape, y_train.shape))
 
-model = build_lenet_model()
+model = build_nvidia_model()
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split = 0.2, shuffle = True, epochs = 5)
+model.fit(X_train, y_train, validation_split = 0.2, shuffle = True, epochs = 8)
 model.save('model.h5')
